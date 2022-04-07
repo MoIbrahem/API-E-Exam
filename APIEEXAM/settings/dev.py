30b2e4bf2e21934e0183b1b5b1500cdc9fbd9097
Mohamed Ibrahem <mh18399@gmail.com>
2022-04-07 06:39:55 +0200

    fix
    Signed-off-by: Mohamed Ibrahem <mh18399@gmail.com>

---
+++ APIEEXAM/settings/dev.py
@@ -0,0 +1,21 @@
+from common import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-=d3bt%xpyorn27t9fh+-t3wp=t)4-k!(2a7@!^arh*nu%lp^y$'
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront3',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1cssdsdsdassdsc'
+    }
+}
