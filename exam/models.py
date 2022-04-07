a247b6eeca5d999cc13553e09f93943c4463315e
Mohamed Ibrahem <mh18399@gmail.com>
2022-04-08 01:39:48 +0200

    added models
    Signed-off-by: Mohamed Ibrahem <mh18399@gmail.com>

--- exam/models.py
+++ exam/models.py
@@ -1,3 +1,4 @@
+from importlib.util import decode_source
 from django.contrib import admin
 from django.conf import settings
 from django.core.validators import MinValueValidator
@@ -8,4 +9,101 @@ from model_utils.managers import InheritanceManager
 # Create your models here.
 # Ramadan Kareem
 
-#comment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Chapter(models.Model):
+    title = models.CharField(max_length=255)
+    # question = models.ForeignKey(
+    #     'Question', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    chapter = models.ForeignKey(Chapter, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+class Answer(models.Model):
+    title = models.CharField(max_length=255)
+    question = models.ManyToManyField(Question, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+
+
+class Subject(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(null=True, blank=True)
+    hours = models.IntegerField(validators=[MinValueValidator(1)])
+    level = models.ForeignKey(Level, on_delete=models.PROTECT)
+    departments = models.ManyToManyField(Department, on_delete=models.PROTECT)
+
+    
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
