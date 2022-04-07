cc75c87800ab2c3022789381529aa559c4501259
Mohamed Ibrahem <mh18399@gmail.com>
2022-04-07 07:16:19 +0200

    models imports
    Signed-off-by: Mohamed Ibrahem <mh18399@gmail.com>

--- exam/models.py
+++ exam/models.py
@@ -1,3 +1,8 @@
+from django.contrib import admin
+from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
+from uuid import uuid4
+from model_utils.managers import InheritanceManager
 
 # Create your models here.
