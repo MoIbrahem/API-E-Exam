e85740527709944136758cacec8bb6c10d9f7a2d
Mo-Adel1 <mohammedadel543210@yahoo.com>
2022-04-08 01:42:46 +0200

    Create Levels and Departments Data Models

--- exam/models.py
+++ exam/models.py
@@ -8,4 +8,21 @@ from model_utils.managers import InheritanceManager
 # Create your models here.
 # Ramadan Kareem
 
-#comment
+class Level(models.Model):
+    title = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+            return self.title
+
+    class Meta:
+        ordering = ['title']
+
+class Department(models.Model):
+    title = models.CharField(max_length=255)
+    levels = models.ManyToManyField(Level)
+    
+    def __str__(self) -> str:
+            return self.title
+
+    class Meta:
+        ordering = ['title']
