e11c4191446f547ae1cb7e460e4c3ae3b4bb7327
Mo-Adel1 <mohammedadel543210@yahoo.com>
2022-04-08 02:34:52 +0200

    Persons Data Models Added

--- exam/models.py
+++ exam/models.py
@@ -79,3 +79,32 @@ class Subject(models.Model):
 
     class Meta:
         ordering = ['title']
+
+class Person(models.Model):
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True, blank=True)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
+
+class Student(Person):
+    #results = models.OneToManyField(Result, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+    class Meta:
+        ordering = ['user__first_name', 'user__last_name']
+
+class Professor(Person):
+    subjects = models.ManyToManyField(Subject, on_delete=models.PROTECT)
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+    class Meta:
+        ordering = ['user__first_name', 'user__last_name']
+        
