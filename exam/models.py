2924abc25fb5921e18cb1b3a78e714c7335844be
Mohamed Ibrahem <mh18399@gmail.com>
2022-04-09 10:11:50 +0200

    edited questions moded and added exam model and exam questions
    Signed-off-by: Mohamed Ibrahem <mh18399@gmail.com>

--- exam/models.py
+++ exam/models.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.contrib import admin
 from django.conf import settings
 from django.core.validators import MinValueValidator
@@ -43,9 +44,8 @@ class Chapter(models.Model):
     class Meta:
         ordering = ['title']
 
+class Difficulty(models.Model):
 
-
-class Question(models.Model):
     DEFICULTY_EASY = 'E'
     DEFICULTY_MEDIUM = 'M'
     DEFICULTY_HARD = 'H'
@@ -56,10 +56,48 @@ class Question(models.Model):
         (DEFICULTY_HARD, 'Hard'),
     ]
 
+
+    title =  models.CharField(
+        max_length=1, choices=DEFICULTY_CHOICES, default=DEFICULTY_MEDIUM)
+    # question = models.ForeignKey(
+    #     'Question', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+class Type(models.Model):
+
+    TYPE_TRUE_OR_FALSE = 'TOF'
+    TYPE_MCQ = 'MCQ'
+
+    TYPE_CHOICES = [
+        (TYPE_TRUE_OR_FALSE, 'True or false'),
+        (TYPE_MCQ, 'MCQ'),
+    ]
+
+    title =  models.CharField(
+        max_length=3, choices=TYPE_CHOICES)
+    # question = models.ForeignKey(
+    #     'Question', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+
+class Question(models.Model):
+    
     title = models.CharField(max_length=255)
     chapter = models.ForeignKey(Chapter, on_delete=models.PROTECT)
-    deficulty = models.CharField(
-        max_length=1, choices=DEFICULTY_CHOICES, default=DEFICULTY_MEDIUM)
+    difficulty = models.ForeignKey(Difficulty, on_delete=models.PROTECT)
+    type = models.ForeignKey(Type, on_delete=models.PROTECT)
+    
     def __str__(self) -> str:
         return self.title
 
@@ -126,6 +164,32 @@ class Professor(Person):
         ordering = ['user__first_name', 'user__last_name']
         
 class RightAnswer(models.Model):
-    questions = models.ForeignKey(Question, on_delete=models.PROTECT)
+    questions = models.ForeignKey(Question, on_delete=models.CASCADE)
     answers = models.ManyToManyField(Answer, on_delete=models.PROTECT)
+
+class Exam(models.Model):
+    title = models.CharField(max_length=255)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    starts_at = models.DateTimeField(blank=True)
+    ends_at = models.DateTimeField(blank=True) 
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+class ExamQuestion(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE, related_name='questions')
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE, related_name='chapters')
+    difficulty = models.ForeignKey(Difficulty, on_delete=models.CASCADE)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(validators=[MinValueValidator(1)])
+    
+    class Meta:
+        unique_together = [['exam', 'chapter','difficulty','type','quantity']]
+
+
+    
 
