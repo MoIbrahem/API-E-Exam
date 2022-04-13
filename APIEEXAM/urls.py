de87316ae111f6f83f7d58b9e08b65348f5ecc5e
Mohamed Ibrahem <mh18399@gmail.com>
2022-04-13 12:02:00 +0200

    url djoser
    Signed-off-by: Mohamed Ibrahem <mh18399@gmail.com>

--- APIEEXAM/urls.py
+++ APIEEXAM/urls.py
@@ -24,5 +24,7 @@ admin.site.index_title = 'Admin'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
