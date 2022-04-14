658a093e7393176335f5e93cf88c286969c2a96c
Mohamed Ibrahem <mh18399@gmail.com>
2022-04-14 08:11:10 +0200

    refactored file
    Signed-off-by: Mohamed Ibrahem <mh18399@gmail.com>

--- exam/serializers.py
+++ exam/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import fields, serializers
 # from .signals import order_created
 # from core.serializers import *
 # from core.models import *
-from .models import Exam, Question
+from .models import Exam, Question, Result, Student
 
 
 class QuestionSerializer(serializers.ModelSerializer):
@@ -17,7 +17,7 @@ class ExamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exam
         fields = ['id', 'title', 'subject', 'starts_at', 'ends_at']
-from .models import Result, Student
+
 
 
 
