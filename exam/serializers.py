f89480d2a7cc9268d62eabac7d9ea4c49364f366
Mohamed Ibrahem <mh18399@gmail.com>
2022-04-15 11:37:28 +0200

    updates
    Signed-off-by: Mohamed Ibrahem <mh18399@gmail.com>

--- exam/serializers.py
+++ exam/serializers.py
@@ -1,25 +1,67 @@
+from asyncore import read
 from decimal import Decimal
 from django.db import transaction
 from rest_framework import fields, serializers
 # from .signals import order_created
 # from core.serializers import *
 # from core.models import *
-from .models import Exam, Question, Result, Student
+from .models import Exam, ExamQuestion, Question, Result, Student, Subject
 
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = ['id', 'title', 'type', 'answer']
+        # fields = ['id', 'title', 'type', 'answer']
+        fields = '__all__'
 
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = [ 'id','title', 'hours']
+        read_only_fields = [ 'id','title', 'hours']
+class ExamQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExamQuestion
+        fields= ['question']
+        
+    question = QuestionSerializer()
+
+class CreateExamQuestionSerializer(serializers.Serializer):
+    exam__id = serializers.IntegerField()
+
+    def save(self, **kwargs):
+        exam__id = self.validated_data['exam__id']
+        print(self.validated_data['exam__id'])
+        print(self.context['user_id'])
+        sub = Exam.objects.only('subject').get(id= exam__id)
+        # qusestions = Question.objects.filter(subject_id = sub.id)
+        return sub
+
+    def validate_exam_id(self, exam__id):
+        if not Exam.objects.filter(pk=exam__id).exists():
+            raise serializers.ValidationError(
+                'No exam with the given ID was found.')
+        if Exam.objects.filter(exam_id=exam__id).count() == 0:
+            raise serializers.ValidationError('The exam is empty.')
+        return 'ok'
+    
+        
+    
+    
 
 class ExamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exam
         fields = ['id', 'title', 'subject', 'starts_at', 'ends_at']
-
-
-
+        # fields = ['id', 'title', 'subject', 'starts_at', 'ends_at', 'examquestion']
+        read_only_fields = ['id', 'title', 'subject', 'starts_at', 'ends_at']
+    subject = SubjectSerializer()
+    
+class SimpleExamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exam
+        fields = ['subject']
+        read_only_fields = ['subject']
 
 class StudentSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField(read_only=True)
--- exam/views.py
+++ exam/views.py
@@ -1,13 +1,15 @@
+from operator import truediv
+from venv import create
 from rest_framework import response
 from exam.permissions import FullDjangoModelPermissions, IsAdminOrReadOnly, ViewStudentHistoryPermission
 from exam.pagination import DefaultPagination
 from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.decorators import action, permission_classes
+from rest_framework.decorators import action, permission_classes, api_view
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
-from rest_framework.permissions import AllowAny, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAdminUser, IsAuthenticated,IsAuthenticatedOrReadOnly, AllowAny, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework import status
@@ -43,9 +45,18 @@ class StudentViewSet(ModelViewSet):
             return Response(serializer.data)
 
 class ExamViewSet(ModelViewSet):
+    http_method_names = ['get', 'patch', 'delete', 'head', 'options']
     
     serializer_class = ExamSerializer
-    permission_classes = [IsAuthenticated]
+    
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsAdminUser()]
+        return [IsAuthenticatedOrReadOnly()]
+
+    
+
     def get_queryset(self):
 
         user = self.request.user
@@ -62,8 +73,46 @@ class ExamViewSet(ModelViewSet):
         
 
 
-        return Exam.objects.filter(subject_id__in =subjects)  
+        return Exam.objects.select_related('subject').filter(subject_id__in =subjects)  
+
+class ExamQuestionViewSet(ModelViewSet):
+    http_method_names = ['get','post', 'delete', 'head', 'options']
+    
+    def get_permissions(self):
+        if self.request.method in ['DELETE']:
+            return [IsAdminUser()]
+        return [IsAuthenticatedOrReadOnly()]  
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateExamQuestionSerializer
+        return ExamQuestionSerializer
+    
+    def get_serializer_context(self):
+        return {'user_id':self.request.user.id,}
+    
+    def create(self, request, *args, **kwargs):
+        serializer = CreateExamQuestionSerializer(data=request.data,
+        context= {'user_id':self.request.user.id}) 
+        serializer.is_valid(raise_exception=True)
+        sub = serializer.save()
+        serializer = SimpleExamSerializer(sub)
+        return   Response(serializer.data)
         
+    
+    def get_queryset(self):
+        user = self.request.user
+        sub__id = create(self,self.request)
         
+        # subject_id = Subject.objects.only('id').get(exam = ex)
+        # return Subject.objects.select_related('questions').filter(subject_id= subject_id)
+
+
+
+
+    
+
+         
         
+
         
