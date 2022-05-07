145015f99d16e572563eda786e076a5041cb8279
Mohamed Ibrahem <mh18399@gmail.com>
2022-05-07 07:24:33 +0200

    added email service in dev settings
    Signed-off-by: Mohamed Ibrahem <mh18399@gmail.com>

--- APIEEXAM/settings/dev.py
+++ APIEEXAM/settings/dev.py
@@ -19,3 +19,10 @@ DATABASES = {
         'PASSWORD': ''
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
