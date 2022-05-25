9541fe04139a98a35e8d9e02f44a92657843521f
Mo-Adel1 <mohammedadel543210@yahoo.com>
2022-05-25 04:11:36 +0200

    "add to staff" & "remove from staff" actions of professors  added

--- core/admin.py
+++ core/admin.py
@@ -6,6 +6,7 @@ from .models import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
+    actions = ['add_to_staff_members', 'remove_from_staff_members']
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
@@ -13,3 +14,20 @@ class UserAdmin(BaseUserAdmin):
         }),
     )
     list_filter = ['profile_type', 'is_staff', 'is_superuser', 'is_active']
+    @admin.action(description='Add to staff members')
+    def add_to_staff_members(self, request, queryset):
+        added_to_staff = queryset.update(is_staff=True)
+        self.message_user(
+            request,
+            f'{added_to_staff} professor successfully added to staff members.'
+        )
+    
+    @admin.action(description='Remove from staff members')
+    def remove_from_staff_members(self, request, queryset):
+        removed_from_staff = queryset.update(is_staff=False)
+        self.message_user(
+            request,
+            f'{removed_from_staff} professor successfully removed from staff members.'
+        )
+    
+
